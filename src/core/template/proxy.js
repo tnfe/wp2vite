91f1461f66e47f9f60bdb6a3338bb8455f591cb5
dravenwu <dravenwu@tencent.com>
2021-03-29 16:22:22 +0800

    update proxy

--- src/core/template/proxy.js
+++ src/core/template/proxy.js
@@ -1,8 +1,13 @@
 // 开发环境下所对应的接口代理，与webpack的一致。
 export default {
-  '/api': {
-    target: 'http://api.qq.com/api/v1',
+  '^/api/.*': {
+    target: 'http://api.qq.com',
     changeOrigin: true,
-    rewrite: path => path.replace(/^\/api/, '') // 将 /api 重写为空
+    rewrite: path => path.replace(/^\/api/, '/apis') // 将 /api 重写为/apis
+  },
+  '^/api2/.*': {
+    target: 'http://api.qq.com',
+    changeOrigin: true,
+    rewrite: path => path.replace(/^\/api2/, '') // 将 /api2 重写为空
   }
 }

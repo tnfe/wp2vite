4b74d04c0b577dc7f184fa26799a7647d64cd3e9
dravenwu <dravenwu@tencent.com>
2021-03-29 16:44:08 +0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # wp2vite
+一款转化工具，可以一键让webpack项目支持vite
+
+tips：目前暂时只支持create-react-app创建的项目
+
+# 安装
+```
+npm install -g wp2vite
+```
+# 使用
+```
+cd yourwork/your_project
+wp2vite --type=create-react-app
+
+npm install
+
+npm run dev
+or
+npm run vite-dev
+```
+
+# todo
+- 支持vue-cli创建的项目
+- 支持自定义配置创建的webpack项目的转化
--- src/core/debug.js
+++ src/core/debug.js
@@ -1,22 +1,21 @@
 const chalk  = require('chalk');
 let debugSwitch = false;
 
+chalk.green('hello wp2vite');
+
 function switchDebug(debug) {
   debugSwitch = debug;
 }
 
 function debugError(msg) {
-  console.log('error::::' + msg)
   debugSwitch && chalk.red(msg);
 }
 
 function debugInfo(msg) {
-  console.log('info::::' + msg)
   debugSwitch && chalk.green(msg);
 }
 
 function debugWarning(msg) {
-  console.log('warning::::' + msg)
   debugSwitch && chalk.yellow(msg);
 }
 
